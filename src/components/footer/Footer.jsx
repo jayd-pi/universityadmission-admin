6004642a402b0fd10ed2ca11addd8a2d01008b15
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-25 18:29:23 +0700

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="6" className="mb-4">
+            <div className="logo">
+              <div>
+                <h1 className="text-white">Efurniture</h1>
+              </div>
+            </div>
+            <p className="footer__text mt-4">
+              Tên công ty: Công ty CP một thành viên Lofi Furniture<br />
+              Địa chỉ: Lô E2a-7, Đường D1, Đ. D1, Long Thạnh Mỹ, Thành Phố Thủ Đức, Thành phố Hồ Chí Minh 700000<br />
+              Email: efurniture@gmail.com<br />
+              Hotline: 1900 8098
+            </p>
+          </Col>
+
+          <Col lg="3" md="3" className="mb-4">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="2" md="3" className="mb-4">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-map-pin-line"></i>
+                  </span>
+                  <p> Lô E2a-7, Đường D1, Đ. D1, Long Thạnh Mỹ, Thành Phố Thủ Đức, Thành phố Hồ Chí Minh 700000</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-phone-line"></i>
+                  </span>
+                  <p>028 7300 5588</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-mail-line"></i>
+                  </span>
+                  <p>efurniture@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer__copyright">
+              Copyright {year} developed by Efurniture. @All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
