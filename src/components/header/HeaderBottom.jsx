1970331e196af851f39fd3b5c4dfb4d52f2e03c2
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-27 10:28:01 +0700

    login page

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -141,7 +141,7 @@ const HeaderBottom = () => {
                 transition={{ duration: 0.5 }}
                 className="absolute top-6 left-0 z-50 bg-primeColor w-44 text-[#767676] h-auto p-4 pb-6"
               >
-                <Link to="/signin">
+                <Link to="/login">
                   <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer">
                     Login
                   </li>
