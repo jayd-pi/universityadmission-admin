28e4dc812a80c601ac8f18a368c16cbd430c2757
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-20 23:28:29 +0700

    fix sampleNextArrow

--- src/components/home/newArrivals/SampleNextArrow.jsx
+++ src/components/home/newArrivals/SampleNextArrow.jsx
@@ -6,7 +6,7 @@ const SampleNextArrow = (props) => {
   const { onClick } = props;
   return (
     <div
-      className="w-10 h-10 rounded-full text-white bg-black bg-opacity-40 hover:bg-opacity-100 duration-300 cursor-pointer flex justify-center items-center z-10 absolute top-[35%] right-2"
+      className="w-10 h-10 rounded-full text-white bg-black bg-opacity-40 hover:bg-opacity-100 duration-300 cursor-pointer flex justify-center items-center z-10 absolute top-[35%] right-6"
       onClick={onClick}
     >
       <span className="text-xl">
