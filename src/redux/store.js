1a22bc19455574c1d66c6c2b809494f60eeeb69c
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-27 20:41:24 +0700

    fix store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistReducer,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import productSlice from "./slice/productSlice";
-import cartSlice from "./slice/cartSlice";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import productSlice from './slice/productSlice';
+import cartSlice from './slice/cartSlice';
+import authReducer from './slice/auth';
+import messageReducer from './slice/message';
 
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
+// Combine the reducers from both configurations
+const rootReducer = combineReducers({
+  product: persistReducer({ key: 'product', storage }, productSlice),
+  cart: cartSlice,
+  auth: authReducer,
+  message: messageReducer
+});
 
-const persistedReducer = persistReducer(persistConfig, productSlice);
-
+// Configure the store with the combined rootReducer
 const store = configureStore({
-  reducer: { productSlice: persistedReducer, cart: cartSlice },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
+  devTools: true,
 });
 
 export default store;
-
