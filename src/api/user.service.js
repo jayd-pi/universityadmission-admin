f28e129203de66b2543efd06f5fe923dd35bc0a8
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-03-03 15:55:29 +0700

    fix port api

--- src/api/user.service.js
+++ src/api/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8000/api/v1/auth/";
+const API_URL = "http://localhost:8080/api/v1/auth/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
